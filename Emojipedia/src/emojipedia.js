9267daaa335ad5c9a268c8c58a6b76189a66cedb
Sana1228 <73459001+Sana1228@users.noreply.github.com>
2021-09-11 12:11:46 +0530

    Add files via upload

---
+++ Emojipedia/src/emojipedia.js
@@ -0,0 +1,67 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id: 5,
+    emoji: "😅",
+    name: "Smiling Face With Open Mouth & Cold Sweat",
+    meaning:
+      "Sweaty smile. Relief that a tight situation went well or was coped with positively, e.g. an important appointment. A difficult event is imminent and you are already nervous."
+  },
+  {
+    id: 6,
+    emoji: "😆",
+    name: "Smiling Face With Open Mouth And Closed Eyes",
+    meaning:
+      "Emoji is on the verge of a fit of laughter because something is so silly or incredibly funny. Also, mischievous or bitchy laughter about an event, or something that happened to another person.. Means the same as “XD“ and “X“ as a text-based emoticon."
+  },
+  {
+    id: 7,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning:
+      "Would like to express its (possibly pretended) innocence. Someone does good deeds, is very sweet, innocent and behaves in an exemplary way. The angelic face can also be used humorously for rather not so good deeds or behaviors."
+  },
+  {
+    id: 8,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "Smiley blinks mischievously with one eye. Expression of humor, has been pulling a practical joke, is flirting with you or has an ulterior motive. The wink weakens the message: do not take it seriously, regard it as humorous."
+  },
+  {
+    id: 9,
+    emoji: "😍",
+    name: "Smiling Face With Heart-Eyes",
+    meaning:
+      "Happily beaming face with heart-shaped eyes. Insanely in love: with a person, a place or an object. Expresses the extent of love and affection as well as gratitude and is often part of romantic messages. Just does not know what to do with all the luck."
+  }
+];
+
+export default emojipedia;
